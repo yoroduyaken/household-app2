fc9819274b93371200f7497382f0980cd88e7836
k_nakashima <yoroduya.kento@gmail.com>
2022-06-06 21:33:15 +0900

    「/」でアクセスできるトップページを作成した。

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>トップ画面</title>
+</head>
+<style>
+body
+    { font-size:16px; color:#999; }
+    h1 { font-size:100pt; text-align:right; color:#eee; 
+        margin:-20px 0px -50px 0px; }
+</style>
+<body>
+    <h1>Household 2</h1>
+    <p>This is Top Page.</p>
+</body>
+</html>
