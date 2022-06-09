217e9c0c417307e3a16d33bd82b13372c0958220
k_nakashima <yoroduya.kento@gmail.com>
2022-06-09 21:42:33 +0900

    「/register」にて登録画面を作成した。

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/home', function () {
     return view('dashboard');
 })->name('home');
 
+Route::get('/register', [App\Http\Controllers\RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', [App\Http\Controllers\RegisterController::class, 'store'])
+    ->middleware('guest');
