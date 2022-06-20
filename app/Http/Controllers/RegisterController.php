1089d486a5887b6d8e983bd99d0cade4f06ee2f9
k_nakashima <yoroduya.kento@gmail.com>
2022-06-21 07:16:03 +0900

    インデントを揃え、不要な「password_confirmation」のバリデーションを削除した。

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,10 +18,9 @@ class RegisterController extends Controller
     public function store(Request $request)
     {
        $request->validate([
-        'name' => 'required|string|max:255|min:8',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|confirmed|min:8',
-        'password_confirmation' => 'required|string|confirmed|min:8',
+            'name' => 'required|string|max:255|min:8',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
        ]);
        
         $user = User::create([
