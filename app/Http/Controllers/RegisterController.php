6101ac9d3e3da0d654f15cd9db2d1a90c70378f3
k_nakashima <yoroduya.kento@gmail.com>
2022-06-20 21:48:37 +0900

    「ユーザー名」「メールアドレス」「パスワード」「パスワード確認用」の項目をバリデーションした。

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,6 +17,13 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
+       $request->validate([
+        'name' => 'required|string|max:255|min:8',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|confirmed|min:8',
+        'password_confirmation' => 'required|string|confirmed|min:8',
+       ]);
+       
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
