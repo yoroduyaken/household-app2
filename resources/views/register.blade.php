e4415f136f754cd522b963aef0aa481d9a6ad4da
k_nakashima <yoroduya.kento@gmail.com>
2022-06-24 22:21:48 +0900

    内容に不備がある時に「フォームの要素」を案内出来るようにした。

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,21 +5,29 @@
     <title>ユーザー登録フォーム</title>
 </head>
 <body>
-    <form action="/register" method="post">
+    <form action="/register"  method="post">
         {{ csrf_field() }}
-        <p>名前</p>
-        <input type="text" name="name" size="30">
-
-        <p>メールアドレス</p>
-        <input type="email" name="email" size="30">
-
-        <p>パスワード</p>
-        <input type="password" name="password" size="30">
-
-        <p>パスワード確認</p>
-        <input type="password" name="password_confirmation" size="30">
-
-        <button type="submit">登録</button>
+    <dl>
+        <dt>名前</dt>
+        <dd><input type="text" name="name" size="30">
+            <span>{{ $errors->first('name') }}</span></dd>
+    </dl>
+    <dl>
+        <dt>メールアドレス</dt>
+        <dd><input type="email" name="email" size="30">
+            <span>{{ $errors->first('email') }}</span></dd>
+    </dl>
+    <dl>
+        <dt>パスワード</dt>
+        <dd><input type="password" name="password" size="30">
+            <span>{{ $errors->first('password') }}</span></dd>
+    </dl>
+    <dl>
+        <dt>パスワード確認</dt>
+        <dd><input type="password" name="password_confirmation" size="30">
+            <span>{{ $errors->first('password_confirmation') }}</span></dd>
+    </dl>
+        <button type="submit" name='action'>登録</button>
     </form>
 </body>
 
