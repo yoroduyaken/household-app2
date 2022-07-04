bbcb01a64374544fdefcf0572ccf89323357ffec
k_nakashima <yoroduya.kento@gmail.com>
2022-07-04 21:13:18 +0900

    ログイン画面に情報が不足していた場合、エラーメッセージが表示されるようにした。

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,10 +17,15 @@ class LoginController extends Controller
   
     public function store(Request $request)
     {
+        $request->validate([
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+       ]);
+
         if (!auth()->attempt($request->only('email', 'password'))) {
-            return redirect()->back();
+            return redirect()->back();       
         }
-        
+
         return redirect()->route('home');
     }
 }
