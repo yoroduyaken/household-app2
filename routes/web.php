0070e88f1b0c75c78f0a12091129db44c616e3b4
k_nakashima <yoroduya.kento@gmail.com>
2022-07-22 21:19:38 +0900

    初期値「use GuzzleHttp\Middleware;」が今回のissueでは不要の為、削除した。

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CostController;
-use GuzzleHttp\Middleware;
 
 /*
 |--------------------------------------------------------------------------
