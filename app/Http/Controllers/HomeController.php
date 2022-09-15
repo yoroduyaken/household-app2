4506bc5c0e882bc5af0cbb63ab5e0e4dce554a96
k_nakashima <yoroduya.kento@gmail.com>
2022-09-15 21:30:37 +0900

    取得できるモデルの数を指定出来るようにした。(5件)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     public function index()
     {
         // $costs = Cost::all();
-        $costs = Cost::orderBy('id', 'desc')->get();
+        $costs = Cost::orderBy('id', 'desc')->take(5)->get();
         return view('home', compact('costs'));
         // return view('home', with(['costs' => $all_costs]));
     }
