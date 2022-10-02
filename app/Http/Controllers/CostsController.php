a096bd39d81450f8e9935ae24b717fd64b8e616e
k_nakashima <yoroduya.kento@gmail.com>
2022-10-02 16:44:07 +0900

    認証しているユーザーのid情報のみ表示出来るように変更した。

--- app/Http/Controllers/CostsController.php
+++ app/Http/Controllers/CostsController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Cost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CostsController extends Controller
 {
     public function index()
     {
-        $costs = Cost::where('user_id','=','1')->get();
+        $costs = Cost::where('user_id', Auth::user()->id)->get();
+        
         return view('auth.costs',compact('costs'));
     } 
 }
